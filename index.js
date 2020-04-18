c3ab17172242ec304690d2b4ef5da44e1a8868b7
bjsamuel <chinnabhai944@gmail.com>
2020-04-18 12:06:40 +0530

    test 1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express'
 
 const app = express()
 const MongoUrl = process.mongodbUrlCluster0;
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 app.use(cors())
 app.listen(3000 || process.env.PORT,()=>{console.log("connected to port:"+process.env.PORT)})
 app.get('/', function (req, res) {
