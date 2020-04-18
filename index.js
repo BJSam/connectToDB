4ef0001a25ffac4ab24cf26f1fafd3f4430f4bf5
bjsamuel <chinnabhai944@gmail.com>
2020-04-18 12:01:32 +0530

    added get pg

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ import bodyParser from 'body-parser'
 import express from 'express'
 
 const app = express()
-
+app.use(bodyparser.json())
 app.use(cors())
+app.listen(3000 || process.env.PORT,()=>{console.log("connected to port:"+process.env.PORT)})
+app.get('/', function (req, res) {
+    res.send('Welcome to mongoDB coneect');
+    
+  });
